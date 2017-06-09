c9991031fc0fce0d6bcd52c1f8e67e77e387145f
metallurgical <norlihazmey89@gmail.com>
2017-06-10 06:10:46 +0800

    adding layout and title

--- src/lsb/steganography/java/LSBSteganographyJava.java
+++ src/lsb/steganography/java/LSBSteganographyJava.java
@@ -6,7 +6,13 @@
 package lsb.steganography.java;
 
 import javafx.application.Application;
-import javafx.scene.layout.GridPane;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 
 /**
@@ -18,7 +24,24 @@ public class LSBSteganographyJava extends Application {
     @Override
     public void start( Stage stage ) {
         
-        GridPane grid = new GridPane();
+        // create root node for title
+        HBox hbox = new HBox(8);
+        hbox.setAlignment( Pos.TOP_CENTER );
+        // padding the title
+        hbox.setPadding( new Insets( 15, 10, 15, 10 ) );
+        
+        Label mainTitle = new Label( "LSB-STEGANOGRAPHY" );
+        mainTitle.setFont( Font.font( "Verdana", FontWeight.BOLD, 30 ) );
+        // adding child node into root node
+        hbox.getChildren().add( mainTitle );
+        
+        Scene scene = new Scene( hbox, 300, 100 );
+        stage.setScene( scene );
+        // make default width and height
+        stage.setMinWidth( 400 );
+        stage.setMinHeight( 300 );
+        stage.show();
+        
     }
 
     /**
