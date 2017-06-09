af000c33ee570f0c956967cee53ccc43fd99bbdf
metallurgical <norlihazmey89@gmail.com>
2017-06-10 05:42:14 +0800

    init

---
+++ src/lsb/steganography/java/LSBSteganographyJava.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lsb.steganography.java;
+
+import javafx.application.Application;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author metallurgical
+ */
+public class LSBSteganographyJava extends Application {
+    
+    @Override
+    public void start( Stage stage ) {
+        
+        GridPane grid = new GridPane();
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main( String[] args ) {
+        launch( args );
+    }
+    
+}
